1b33a7425d8bbeef33038b449b94c5cb82d4ba1d
suba <suba>
2023-08-25 14:23:42 -0400

    commit master branch

---
+++ testGit/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author subas
+ *
+ */
+module testGit {
+}
---
+++ testGit/src/testGit/Addition.java
@@ -0,0 +1,10 @@
+package testGit;
+
+public class Addition {
+	public static void main(String args[]) {
+		
+		System.out.println("addition");
+		
+	}
+
+}
